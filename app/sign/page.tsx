5d0e5c7931ae36996f7457c91a89cfa4c57b924f
nwachter <malaikaninaw@yahoo.fr>
2025-03-06 14:11:06 +0100

    export const... export default XXX turned to export default function in sign/page.tsx

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -20,24 +20,7 @@ type FormFieldProps = {
     }>>
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-export const FormField: React.FC<FormFieldProps> = ({ icon, error, setInputs, ...props }) => (
-    <div className="flex flex-col">
-        <div className="relative">
-            <div className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-                {icon}
-            </div>
-            <input
-                {...props}
-                onChange={e => setInputs((prev: any) => ({...prev, [e.target.name]: e.target.value}))}
-                className="w-full pl-10 pr-4 py-2.5 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all"
-            />
-        </div>
-        {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
-    </div>
-);
-
-
-const SignPages = () => {
+export default function SignPages() {
     const [isSignUp, setIsSignUp] = useState(true);
     const [inputs, setInputs] = useState({
         email: "",
@@ -337,5 +320,20 @@ const SignPages = () => {
     );
 };
 
+export const FormField: React.FC<FormFieldProps> = ({ icon, error, setInputs, ...props }) => (
+    <div className="flex flex-col">
+        <div className="relative">
+            <div className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+                {icon}
+            </div>
+            <input
+                {...props}
+                onChange={e => setInputs((prev: any) => ({...prev, [e.target.name]: e.target.value}))}
+                className="w-full pl-10 pr-4 py-2.5 rounded-lg border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all"
+            />
+        </div>
+        {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
+);
 
-export default SignPages;
+
